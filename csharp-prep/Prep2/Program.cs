44c66a81a542649903b90bd29b3c00795313fca9
Kayla Marchant <mar20187@byui.edu>
2024-01-11 20:06:57 -0700

    redid prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -10,25 +10,38 @@ class Program
         string usersInput = Console.ReadLine();
         int usersGrade = int.Parse(usersInput);
 
+        string letter = "";
+
         if (usersGrade >= 90)
         {
-            Console.WriteLine("You have an A in the class.");
+            letter = "A";
         }
         else if (usersGrade >= 80 && usersGrade < 90)
         {
-            Console.WriteLine("You have a B in the class.");
+            letter = "B";
         }
         else if (usersGrade >= 70 && usersGrade < 80)
         {
-            Console.WriteLine("You have a C in the class.");
+            letter = "C";
         }
         else if (usersGrade >= 60 && usersGrade < 70)
         {
-            Console.WriteLine("You have a D in the class.");
+            letter = "D";
         }
         else
         {
-            Console.WriteLine("You have an F in the class, and therefore fail.");
+            letter = "F";
+        }
+
+        Console.WriteLine($"This is your grade: {letter}");
+
+        if (usersGrade >= 70)
+        {
+            Console.WriteLine("Congradulations, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunately, you did not pass. A tutor will be assigned to you to help.");
         }
     }
 }
