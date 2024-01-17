53ca0d5c00f8fd4018f11027941256b7a2517246
Kayla Marchant <mar20187@byui.edu>
2024-01-17 12:39:44 -0700

    did prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -5,5 +5,38 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep5 World!");
+        
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int userNumberSquare = SquareNumber(userNumber);
+        DisplayResult(userName, userNumberSquare);
+
+    }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string theNumber = Console.ReadLine();
+        int number = int.Parse(theNumber);
+        return number;
+    }
+    static int SquareNumber(int userNumber)
+    {
+        int numberSquare = userNumber * userNumber;
+        return numberSquare;
+    }
+    static void DisplayResult(string userName, int userNumberSquare)
+    {
+        Console.Write($"{userName}, the square of your number is {userNumberSquare}");
     }
 }
