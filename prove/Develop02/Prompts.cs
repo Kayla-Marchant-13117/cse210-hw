d2e69d7c904988b7001d40f666173ccaa181e208
Kayla Marchant <mar20187@byui.edu>
2024-01-30 17:05:29 -0700

    go through menu

---
+++ prove/Develop02/Prompts.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+
+public class Prompts
+{
+   string[] prompts = new string[] {"prompt 1", "prompt 2", "prompt 3", "prompt 4", "prompt 5"};
+   
+   
+} 
+
+   
+
