d1b62ce42c7def3371db90e31463b0fc9be5ff97
Kayla Marchant <mar20187@byui.edu>
2024-01-30 17:14:15 -0700

    added prompts

--- prove/Develop02/Prompts.cs
+++ prove/Develop02/Prompts.cs
@@ -4,7 +4,11 @@ using System.Collections.Generic;
 
 public class Prompts
 {
-   string[] prompts = new string[] {"prompt 1", "prompt 2", "prompt 3", "prompt 4", "prompt 5"};
+   string[] prompts = new string[] {"Who was someone you spoke to today? What did you talk about?", 
+                                    "What is something fun you did today?", 
+                                    "What was your favorite animal as a kid and why?", 
+                                    "Who do you feel looks up to you in your life?", 
+                                    "Who do you consider to be your best friend? Write about them."};
    
    
 } 
