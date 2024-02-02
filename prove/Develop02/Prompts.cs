d9bdec7b17f66d0a5449083936665e1e111d2fde
Kayla Marchant <mar20187@byui.edu>
2024-02-01 17:55:36 -0700

    generate random prompt

--- prove/Develop02/Prompts.cs
+++ prove/Develop02/Prompts.cs
@@ -4,11 +4,20 @@ using System.Collections.Generic;
 
 public class Prompts
 {
-   string[] prompts = new string[] {"Who was someone you spoke to today? What did you talk about?", 
+   public string RandomPrompts()
+   {
+      var prompts = new List<string> {"Who was someone you spoke to today? What did you talk about?", 
                                     "What is something fun you did today?", 
                                     "What was your favorite animal as a kid and why?", 
                                     "Who do you feel looks up to you in your life?", 
                                     "Who do you consider to be your best friend? Write about them."};
+      int count = prompts.Count;
+      var random = new Random();
+      int _listIndex = random.Next(count);
+      string _prompt = prompts[_listIndex];
+      return _prompt;
+   }
+   
    
    
 } 
