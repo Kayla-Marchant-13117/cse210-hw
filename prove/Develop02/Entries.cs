9da06103d81bc15185f001e31450754c37f985d3
Kayla Marchant <mar20187@byui.edu>
2024-02-01 17:57:55 -0700

    nothing

---
+++ prove/Develop02/Entries.cs
@@ -0,0 +1,20 @@
+// using System;
+// using System.Reflection.Metadata.Ecma335;
+
+
+
+// public class Entries
+// {
+    
+//     if (Program.userNumber2 == 6)
+//     {
+//         //choose random prompt from prompts
+//     }
+
+//     else if (Program.userNumber2 == 7)
+//     {
+//         Console.WriteLine("Begin Writing: ")
+//         string noPromptEntry = Console.ReadLine();
+//         return noPromptEntry;
+//     }
+// }
